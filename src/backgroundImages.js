c38de31bc6c3f5053ea722cba6d1e0dca12b14cf
sirgalleto <sebastian.osorio@wizeline.com>
2018-06-19 20:28:27 -0500

    Remove created react app

--- src/backgroundImages.js
+++
@@ -1,7 +0,0 @@
-import b1 from './backgrounds/b1.jpg';
-import b2 from './backgrounds/b2.jpg';
-import b3 from './backgrounds/b3.jpg';
-
-const backgroundImages = [b1, b2, b3];
-
-export default backgroundImages;
