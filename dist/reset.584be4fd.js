b99835206e478e7103ed6aa2dafbad53e927bba7
sirgalleto <sebastian.osorio@wizeline.com>
2018-06-22 22:59:41 -0500

    build 1529726381

--- dist/reset.584be4fd.js
+++ dist/reset.584be4fd.js
@@ -169,7 +169,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":17}],25:[function(require,module,exports) {
+},{"_css_loader":17}],26:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '59830' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '59667' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -339,4 +339,4 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[25], null)
+},{}]},{},[26], null)
--- dist/styles.2b817e15.js
+++ dist/styles.2b817e15.js
@@ -169,7 +169,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../src/images/street-art.JPG":33,"_css_loader":17}],25:[function(require,module,exports) {
+},{"./../src/images/street-art.JPG":33,"_css_loader":17}],26:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '59830' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '59667' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -418,4 +418,4 @@ LazyPromise.prototype.catch = function (onError) {
 };
 },{"./bundle-url":21}],0:[function(require,module,exports) {
 var b=require(19);b.load([["street-art.b9bfd9a2.JPG",33]]);
-},{}]},{},[25,0], null)
+},{}]},{},[26,0], null)
